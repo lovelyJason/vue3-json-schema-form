a1e541da672877ac34964e7e0a11ccb8086179e8
lovelyJason <1248394650@qq.com>
2024-03-24 19:47:56 +0800

    feat: 构建为lib，修改了scripts脚本和vite config;并使用npm-run-all设置启动脚本

---
+++ lib/theme-default/index.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ThemeDefault',
+  setup(props, ctx) {
+    return () => {
+      return null
+    }
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import circleDependency from 'vite-plugin-circular-dependency'
+import path from 'node:path'
+import fs from 'node:fs'
+
+const outputPath = process.env.name || ''
+let entry = path.resolve(__dirname, 'lib', outputPath, 'index.ts')
+
+if (!fs.existsSync(entry)) {
+  if (!fs.existsSync(path.resolve(__dirname, 'lib', outputPath, 'index.tsx'))) {
+    throw Error('入口文件不存在')
+  } else {
+    entry = path.resolve(__dirname, 'lib', outputPath, 'index.tsx')
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +24,25 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: entry,
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
     }
   }
 })
